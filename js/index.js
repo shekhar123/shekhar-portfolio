da6490d6384bbf4ded25a02ca29507477f863aff
shekhar123 <shekhar.aryal123@gmail.com>
2017-09-26 18:12:07 +0300

    updated

--- js/index.js
+++ js/index.js
@@ -3,18 +3,6 @@ var skillName =[["JavaScript", 4],["JSON/Ajax", 4],["HTML/HTML5", 4],["JQuery", 
                 ["CSS", 4],["Unix", 3],["Java & C", 3],
                 ["Bootstrap", 4],["Wordpress CMS", 3] ];
 var language =[["Englis",4],["Finnish: YKI Certified",3],["Hindi",4],["Nepali",5]];
-
-$(document).ready(function(){
-  console.log("Item :");
-  for (var i = 0; i < skillName.length; i++){
-      console.log("Course:"+skillName[i][0] + "Grade:"+skillName[i][1]);
-      renderHTML(skillName[i][0],skillName[i][1],"skill");
-  }
-  for (var i = 0; i < language.length; i++){
-      renderHTML(language[i][0],language[i][1],"language");
-  }
-});
-
 function renderHTML(array0, array1,temStr){
   var fa= "";
   var htmlE='<p> <strong>'+array0+'</strong><span style="float:right;">';
@@ -30,5 +18,39 @@ function renderHTML(array0, array1,temStr){
   else{
       $('#language').append(htmlE+fa);
   }
-
 }
+function smoothScroll(){
+  // Add smooth scrolling to all links in navbar + footer link
+ $(".navbar a").on('click', function(event) {
+   // Make sure this.hash has a value before overriding default behavior
+   if (this.hash !== "") {
+     // Prevent default anchor click behavior
+     event.preventDefault();
+
+     // Store hash
+     var hash = this.hash;
+
+     // Using jQuery's animate() method to add smooth page scroll
+     // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+     $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 800, function(){
+
+       // Add hash (#) to URL when done scrolling (default click behavior)
+       window.location.hash = hash;
+     });
+   } // End if
+ });
+}
+
+$(document).ready(function(){
+  smoothScroll();
+  console.log("Item :");
+  for (var i = 0; i < skillName.length; i++){
+      console.log("Course:"+skillName[i][0] + "Grade:"+skillName[i][1]);
+      renderHTML(skillName[i][0],skillName[i][1],"skill");
+  }
+  for (var i = 0; i < language.length; i++){
+      renderHTML(language[i][0],language[i][1],"language");
+  }
+});
